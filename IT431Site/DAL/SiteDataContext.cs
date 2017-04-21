fd1d74a22fd1a9d2920ade753ea25416859bf446
Andrew Fick <afick@i4cinnovations.com>
2017-04-20 20:54:57 -0400

    Add project files.

---
+++ IT431Site/DAL/SiteDataContext.cs
@@ -0,0 +1,14 @@
+﻿using IT431Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace IT431Site.DAL
+{
+    public class SiteDataContext:DbContext
+    {
+        public DbSet<Widget> Widgets { get; set; }
+    }
+}
