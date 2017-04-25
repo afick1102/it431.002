3ec1c561249cb226c13f234ff66d50a4c17369d1
bngo6 <Bryan Ngo@BRYANNGO-HP>
2017-04-25 16:09:57 -0400

    ReservationClass

---
+++ IT431Site/Models/Reservation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IT431Site.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Customer Name")]
+        [StringLength(30)]
+        public string CustomerName { get; set; }
+
+        [Required]
+        [Display(Name = "Customer Phone")]
+        [DataType(DataType.PhoneNumber)]
+        public string CustomerPhone { get; set; }
+
+        [Required]
+        [Display(Name = "Customer Email")]
+        [DataType(DataType.EmailAddress)]
+        public string CustomerEmail { get; set; }
+
+        [Required]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [Display(Name = "Travel Reason")]
+        public string TravelReason { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Adult")]
+        public AdultList AdultNum { get; set; }
+
+        [Required]
+        [Display(Name = "Number of Children")]
+        public ChildrenList ChildrenNum { get; set; }
+
+        [Display(Name = "Special Request")]
+        [StringLength(100)]
+        public string Request { get; set; }
+
+    }
+}
